6ee465654ada00b80f37c469ea557328ff43395a
jairo <jairosxd1992@gmail.com>
2016-09-07 18:29:13 +0200

    App listen to port 80

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var express = require('express');
 var server = express();
 
 server.use('/', express.static(__dirname + '/public'));
-server.listen(8080, function () {
-    console.log('Server running at:8080');
+server.listen(80, function () {
+    console.log('Server running at:80');
 });
