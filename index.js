28d49e85231e12cae600e0e01d21c435f35084d4
jairo <jairosxd1992@gmail.com>
2016-09-07 18:34:42 +0200

    App listen to port 5000

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Server config and run
 var express = require('express');
 var server = express();
-var port = 8080
+var port = 5000
 server.use('/', express.static(__dirname + '/public'));
 server.listen(port, function () {
-    console.log('Server running at:8080');
+    console.log('Server running at:5000');
 });
