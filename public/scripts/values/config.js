c775106611e6fd3fbf1ee93c400028ea908a1f18
jairo <jairosxd1992@gmail.com>
2016-09-07 18:27:46 +0200

    changed API host URL

--- public/scripts/values/config.js
+++ public/scripts/values/config.js
@@ -1,4 +1,4 @@
 angular.module('TradeApp').value('config', {
-    "trade_url": "http://localhost:8000/api/1.0/trades/",
+    "trade_url": "http://sheltered-temple-76266.herokuapp.com/api/1.0/trades/",
     "rate_provider_url":"http://api.fixer.io/latest"
 });
